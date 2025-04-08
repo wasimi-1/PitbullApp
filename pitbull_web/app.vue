03f9764c7b5576f07d36c269f123b1152dd52423
Milosz W. <milosz.wasielewski@icloud.com>
2025-04-08 10:51:00 +0200

    Postawienie projektu Nuxt

---
+++ pitbull_web/app.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <AppHeader />
+    <AppFooter />
+  </div>
+</template>
---
+++ pitbull_web/components/AppFooter.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+
+<template>
+    <div> Footer</div>
+
+</template>
+
+<style scoped>
+
+</style>
