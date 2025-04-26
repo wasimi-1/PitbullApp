8748bf912f410fea00a69bf7e4e153187ec14aab
Milosz W. <milosz.wasielewski@icloud.com>
2025-04-26 20:00:59 +0200

    Wygeerowanie aplikacji Pitbull Web PWA

--- pitbull_web/app.vue
+++ pitbull_web/app.vue
@@ -1,24 +1,31 @@
 <script setup>
+import { ref } from 'vue';
+
+useHead({
+  title: 'Nuxt 3 PWA',
+  link: [
+    { rel: 'manifest', href: '/manifest.webmanifest' }
+  ],
+  meta: [
+    { name: 'theme-color', content: '#4DBA87' }
+  ]
+})
+
 const stateModal = ref(false);
-const modalTitle = ref('');
-const modalText = ref('');
+const startModal = () => {
+    stateModal.value = true
+};
 
-const startModal = ({title = '', text = ''}) => {
-    modalTitle.value = title;
-    modalText.value = text;
-    stateModal.value = true;
-}
 const stopModal = () => {
-    stateModal.value = false;
-}
+    stateModal.value = false
+};
 </script>
-
 <template>
-  <div>
     <BarBlack @toogle-modal="startModal"/>
     <NavBar />
     <NuxtPage />
     <AppFooter />
     <Modal v-if="stateModal" @toogle-modal="stopModal" :title="modalTitle" :text="modalText"/>
-  </div>
 </template>
+<style scoped>
+</style>
--- pitbull_web/nuxt.config.ts
+++ pitbull_web/nuxt.config.ts
@@ -2,11 +2,48 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  css: [
-    'bulma',
-    // CSS file in the project
-    '~/assets/css/main.css',
-    // SCSS file in the project
-    '@fortawesome/fontawesome-free/css/all.css',
-  ]
-})
+  css: ['bulma', '~/assets/css/main.css'],
+  devServer: {
+      https: {
+          key: './localhost+2-key.pem',
+          cert: './localhost+2.pem',
+      },
+  },
+  modules: ['@vite-pwa/nuxt'],
+  pwa: {
+      registerType: 'autoUpdate',
+      manifest: {
+          name: 'Nuxt 3 PWA 2025',
+          short_name: 'Nuxt3PWA',
+          description: 'Demo Nuxt 3 PWA',
+          theme_color: '#4DBA87',
+          background_color: '#ffffff',
+          display: 'standalone',
+          icons: [
+              {
+                  src: 'pitbull_small.png',
+                  sizes: '192x192',
+                  type: 'image/png',
+              },
+              {
+                  src: 'pitbull.png',
+                  sizes: '512x512',
+                  type: 'image/png',
+              },
+              {
+                  src: 'pitbull.png',
+                  sizes: '512x512',
+                  type: 'image/png',
+                  purpose: 'maskable',
+              },
+          ],
+      },
+      workbox: {
+          globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
+      },
+      devOptions: {
+          enabled: true,
+          type: 'module',
+      },
+  },
+});
